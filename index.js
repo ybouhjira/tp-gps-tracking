dee591f27c01b31d37f7beaf47f6a22af1f2cefd
ybouhjira <b.youssef91@gmail.com>
2014-04-25 20:10:24 +0100

    Serving static files

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-var mongoose = require('mongoose');
-var io = require('socket.io').listen('8000');
-var static = require('node-static');
+var minimist = require('minimist')
+  , static = require('node-static')
+  , mongoose = require('mongoose')
+  , io = require('socket.io')
+  , http = require('http');
 
+
+// get port from cli args
+var args = minimist(process.argv.slice(2))
+  , port = parseInt(args.p || args.port || 8000);
+
+
+// serve static files
+var fileServer = new static.Server('./public')
+
+var httpServer = http.createServer(function (req, res) {
+  req.addListener('end', function() {
+    fileServer.serve(req, res);
+  }).resume();
+});
+
+httpServer.listen(port);
+
+// Serve socket.io files
+io.listen(httpServer);
+
+console.log('Application statred at : http://localhost:' + port);
