4f7de732aa2a9d2a2a92c324e6a1693539ddbadd
ybouhjira <b.youssef91@gmail.com>
2014-04-25 18:10:11 +0100

    Added essential node modules

---
+++ README.md
@@ -0,0 +1,3 @@
+## GPS Tracking
+
+Storing users' paths in realtime using mongodb and socket.io
---
+++ index.js
@@ -0,0 +1,4 @@
+var mongoose = require('mongoose');
+var io = require('socket.io').listen('8000');
+var static = require('node-static');
+
